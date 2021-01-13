60ed8e9c78e3a9789732deb81b96d49846fd0606
fimaua <nidastatachna@gmail.com>
2021-01-13 15:44:40 +0200

    selectors

---
+++ src/Components/Selectors.js
@@ -0,0 +1,18 @@
+export const productsSelector = (state) => {
+  return state.products;
+};
+export const productDataSelector = (state) => {
+  return state.productData;
+};
+export const statusSelector = (state) => {
+  return state.status;
+};
+export const newNameSelector = (state) => {
+  return state.newProductName;
+};
+export const newDescriptionSelector = (state) => {
+  return state.newProductDescription;
+};
+export const newPriceSelector = (state) => {
+  return state.newProductPrice;
+};
